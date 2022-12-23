38253ec84bafada7d00330bf40d6898e357f7f9a
Luka Vuković <vukovicluka227@gmail.com>
2022-12-23 08:57:19 +0100

    Cleanup

--- src/utils/calc-helper.ts
+++ src/utils/calc-helper.ts
@@ -3,7 +3,6 @@ import { scores } from "../scores";
 export const calculateScrabbleScore = (word: string): number => {
   const letters = [...word];
 
-  console.log(letters);
   const score = letters.reduce(
     (acc, letter) => acc + scores[letter.toLowerCase() as keyof typeof scores],
     0
